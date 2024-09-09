469888532102b067ddc6b53420834c83818f7f31
PieMenard <pinedo.m.gustavo@gmail.com>
2024-09-08 23:28:35 -0400

    add GET and POST requests

---
+++ src/app/api/blogs/route.ts
@@ -0,0 +1,34 @@
+import { prisma } from '@/app/utils/prisma';
+import { NextRequest, NextResponse } from 'next/server';
+
+async function fetchPosts() {
+  const response = await prisma.post.findMany();
+  return response;
+}
+
+export async function GET() {
+  try {
+    const posts = await fetchPosts();
+    return NextResponse.json({ sucess: true, data: posts });
+  } catch (error) {
+    return NextResponse.json({ message: 'Error', error }, { status: 500 });
+  }
+}
+
+export async function POST(request: NextRequest) {
+  const { title, description } = await request.json();
+  try {
+    const post = await prisma.post.create({
+      data: {
+        title,
+        description,
+      },
+    });
+    return NextResponse.json({ success: true, data: post }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: error },
+      { status: 500 }
+    );
+  }
+}
