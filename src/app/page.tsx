495cfe89bec9dc376822e3449c13a614517567db
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-09-09 15:56:40 -0400

    add list of posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,13 +29,43 @@ export default function Home() {
   console.log(posts);
   return (
     <main className="w-full h-full">
-      <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-4 rounded-lg bg-slate-800 drop-shadow-xl">
+      <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg bg-slate-800 drop-shadow-xl">
         <h1 className="text-slate-200 text-center text-2xl font-extrabold">
           FULL STACK BLOG APP
         </h1>
       </div>
-      <div className="w-full">
-        <Link href="/blog/add"></Link>
+      <div className="flex my-5 ">
+        <Link
+          href="/blog/add"
+          className="md:w-1/6 sm:w-2/4 text-center rounded-md p-2 m-auto bg-slate-200 font-semibold"
+        >
+          Add New Post
+        </Link>
+      </div>
+      <div className="w-full flex flex-col justify-center items-center">
+        {posts?.map((post: Post) => (
+          <div className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-200 flex flex-col justify-center">
+            <div className="flex items-center my-3">
+              <div className="mr-auto">
+                <h2 className="mr-auto font-semibold">{post.title}</h2>
+              </div>
+              <Link
+                href={`/blog/edit/${post.id}`}
+                className="px-4 py-2 text-center bg-slate-900 rounded-md font-semibold text-slate-200"
+              >
+                Edit
+              </Link>
+            </div>
+            <div className="mr-auto my-1">
+              <blockquote className="font-bold text-slate-700">
+                {new Date(post.date).toDateString()}
+              </blockquote>
+            </div>
+            <div className="mr-auto my-1">
+              <h2>{post.description}</h2>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
