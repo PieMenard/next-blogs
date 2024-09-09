157e369d3901dbe39cc9bc5b1fd26175dfe12279
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-09-09 13:40:31 -0400

    fetch blogs in client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
+'use client';
+
+import { Post } from '@prisma/client';
+import { useEffect, useState } from 'react';
+
+async function fetchBlogs() {
+  // const response = await fetch('/http://localhost3000/api/blog', {
+  //   next: {
+  //     revalidate: 10,
+  //   },
+  // });
+  // const data = await response.json();
+  // return data.posts;
+}
+
 export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await fetch('/api/blogs');
+      const results = await response.json();
+      setPosts(results.data);
+    };
+    getPosts();
+  }, []);
+
+  console.log(posts);
   return <div>Home Page</div>;
 }
